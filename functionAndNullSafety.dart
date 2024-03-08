9a89c7456d4817e9cf98d7531bda954a4b4b1560
MMHE <mominhasanemon@gmail.com>
2024-03-08 22:04:32 +0600

    Dart practice 2

--- functionAndNullSafety.dart
+++ functionAndNullSafety.dart
@@ -30,8 +30,53 @@ void main() {
   print(subN(a: 5, b: 6));
 
   print(recursiveFn());
+  print(switchCase(8));
 }
 
+//function
+String? switchCase(double gpaPoint) {
+  //switch case
+  switch (gpaPoint) {
+    case (1 || 2):
+      return "It's one or two";
+      break;
+
+    case 3:
+      return "It's three";
+      break;
+
+    case 4 && 5:
+      return "It will always be false, and this switch-case will return null";
+      break;
+
+    case 6:
+      return "It's six";
+      break;
+
+    case 7:
+      {
+        return "It's seven";
+      }
+      break;
+
+    case 8:
+      return "It's eight";
+      break;
+
+    case 9:
+      return "It's nine";
+      break;
+
+    case 10:
+      return "It's ten";
+      break;
+
+    default:
+      return null;
+  }
+}
+
+//function
 recursiveFn() {
   print("hola");
   return "hello";
